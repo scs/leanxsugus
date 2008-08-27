40340ab7923a5e0f555649a2f2aef0fd0003e2e2
Michael Schwarz <michael.schwarz@scs.ch>
2008-08-27 14:10:30 +0200

    + Added documentation to config.h.

--- config.h
+++ config.h
@@ -4,13 +4,20 @@
 #include "support.h"
 
 struct {
+	/*! @brief Whether to sort each color of sugus respectively. */
 	bool sort_color[4];
+	/*! @brief Whether to sort sugus whose color could not be determined. */
 	bool sort_unknown;
-	uint32 count_color[4];
-	uint32 count_sorted;
-	uint32 count_unknown;
+	/*! @brief Wether we are in calibration mode. */
 	bool calibrating;
+	/*! @brief Valves which should be activated no matter what. */
 	bool valve_override[16];
+	/*! @brief Number of sugus of each color counted so far. */
+	uint32 count_color[4];
+	/*! @brief Number of sugus sorted out so far. */
+	uint32 count_sorted;
+	/*! @brief Number of sugus whose color could not be determined. */
+	uint32 count_unknown;
 } configuration;
 
 void config_write();
